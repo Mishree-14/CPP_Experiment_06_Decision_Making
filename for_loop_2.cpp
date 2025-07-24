f8e96ded2f34150ff456960af00383b51c34a781
Mishree_14 <mishreekalaria14@gmail.com>
2025-07-24 19:01:53 +0530

    Create for_loop_2.cpp

---
+++ for_loop_2.cpp
@@ -0,0 +1,15 @@
+//Mishree Kalaria
+//24070123052
+//Batch: A2
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char SIT;
+    for(int i=0;i<5;i++){
+        cout<<"SIT"<<"\n";
+    }
+    return 0;
+}
